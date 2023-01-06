47013722ff00fa755b35afa94db6929a73e53336
NK <nnkumar2468@gmail.com>
2023-01-06 12:54:16 +0530

    four

--- divisible.py
+++ divisible.py
@@ -1,11 +1,9 @@
-
-n = input ("enter a number: ")
-    
-divisible(n)
-    
 def divisible(n):
-    for i in range(1,n+1):
-        if i % 5 == 0 or i % 7 == 0:
+    for i in range(1, n+1):
+        if i % 5 == 0 and i % 7 == 0:
             yield i
-            print(i)
 
+n = int(input("enter a number: "))
+    
+for i in divisible(n):
+    print(i, end = " ")
