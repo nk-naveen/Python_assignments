c2f543aff2e58c6e3efc0a1fd1e1aeb5b1340d61
NK <nnkumar2468@gmail.com>
2023-01-09 17:19:17 +0530

    changes

--- directory.py
+++ directory.py
@@ -1,19 +1,4 @@
-import sys
 import os
-import sys
-# path = sys.path
-
-# name = str(input("enter a directory"))
-
-# res = []
-# def find_path(name):
-#     # for i in path:
-# if name in path:
-#     for path in os.listdir(name):
-#     # check if current path is a file
-#         if os.path.isfile(os.path.join(name, path)):
-#             res.append(path)
-#     print(res)
 
 def dir_tree(path):
     print(path)
