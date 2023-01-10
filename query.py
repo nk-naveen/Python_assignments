d4f4fa347d17ed84b27c50bc211465824001cc78
Naveen Kumar <83780886+nk-naveen@users.noreply.github.com>
2023-01-10 13:15:05 +0530

    Add files via upload

---
+++ query.py
@@ -0,0 +1,18 @@
+import mysql.connector
+
+mydb = mysql.connector.connect(
+  host="localhost",
+  user="root",
+  password="root",
+  database="sakila"
+)
+
+cur = mydb.cursor()
+
+def fetch():
+    cur.execute("SELECT first_name FROM actor")
+    for i in cur:
+        yield i
+        
+for i in fetch():
+    print(i)
