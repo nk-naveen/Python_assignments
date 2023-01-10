3c36fd2e87b26fe80890540918e72ec14528df0e
Naveen Kumar <83780886+nk-naveen@users.noreply.github.com>
2023-01-10 13:18:28 +0530

    Update query.py

--- query.py
+++ query.py
@@ -18,22 +18,3 @@ def fetch():
 for i in fetch():
     print(i)
 
-import mysql.connector
-
-mydb = mysql.connector.connect(
-  host="localhost",
-  user="root",
-  password="root",
-  database="sakila"
-)
-
-cur = mydb.cursor()
-
-def fetch():
-    cur.execute("SELECT first_name FROM actor")
-    for i in cur:
-        yield i
-        
-for i in fetch():
-    print(i)
-
