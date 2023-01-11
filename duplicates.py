4243a0b7b05ccfa8b5644beead3ab6922eb901b4
Naveen Kumar <83780886+nk-naveen@users.noreply.github.com>
2023-01-11 11:53:06 +0530

    Update duplicates.py

--- duplicates.py
+++ duplicates.py
@@ -1,9 +1,11 @@
-my_list = [12,24,35,24,88,120,155,88,120,155]
+my_list = [12,24,74,585,999,35,24,88,120,155,88,120,155]
 
 print("list with duplicatses" + str(my_list))
 
-my_list = list(set(my_list))
+my_list1 = list(set(my_list))
 
-my_list.sort()
-print("list without duplicates" + str(my_list))
+my_list1.sort(key=my_list.index)
+
+print("list without duplicates" + str(my_list1))
+
 
